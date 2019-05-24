3705dc8bca56305cbc206f7462fc661abdb04157
flubbid <brendanwilson1@gmail.com>
2019-05-24 09:13:45 -0500

    adds config/databse file

---
+++ README.md
@@ -0,0 +1,2 @@
+#
+DATABASE_URL=mongodb+srv://hackerman:<abcd1234>@cluster0-h7ooi.mongodb.net/Just-in-thyme?retryWrites=true
---
+++ config/database.js
@@ -0,0 +1,15 @@
+mongoose.connect(process.env.DATABASE_URL || devConnectionString,{
+
+})
+
+//MongoDB - Check the stitch application for the info on the blog posts that he
+// referenced 
+
+//Talk with the MongoDB presneter on the information thath eposted. 
+// github from speaker: gihub.com/brittonlaroche/mongoDB-Demos
+//cloud.mongodb.com -> stitch -> look into twillio "server side sms"
+//Speak with Britton to see if he will be available for the demo. 
+//Uses trillio to setup responses from cell phones and was able to gather info
+// from the datbase and send reports.
+
+//YOu can now do a databse joint. Transactions are new], he has a tutorial, how to retry in a loop if you have an issue.
