850038d7e3bf0ad1e021d16aa59e10621d474d75
flubbid <brendanwilson1@gmail.com>
2019-05-29 11:37:38 -0500

    adds routes for .create and .show

--- routes/recipes.js
+++ routes/recipes.js
@@ -2,6 +2,9 @@ const router = require('express').Router()
 const recipeCtrl = require('../controllers/recipes')
 
 router.get( '/', recipeCtrl.index)
+router.get('/new', recipeCtrl.new)
+router.post('/', recipeCtrl.create)
+router.get('/:id', recipeCtrl.show)
 
 
 module.exports = router;
