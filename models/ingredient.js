9c5c52013422afde90749dd2a5a762aa211f878d
flubbid <brendanwilson1@gmail.com>
2019-05-29 17:49:59 -0500

    adds ingredients routes for the new, create and addToRecipe

--- models/ingredient.js
+++ models/ingredient.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 const ingredientSchema = new mongoose.Schema({
     id: Number,
     name: String,
-    ingredientCategory: String
 })
 
 module.exports = mongoose.model('Ingredients', ingredientSchema)
--- routes/ingredients.js
+++ routes/ingredients.js
@@ -1,7 +1,9 @@
-const express = require('express').Router();
+const router = require('express').Router();
 const ingredientCtrl = require('../controllers/ingredients')
 
-
+router.get('/performers/new', ingredientCtrl.new);
+router.post('/ingredients', ingredientCtrl.create);
+router.post('/recipes/:id/ingredients', ingredientCtrl.addToRecipe);
 
 module.exports = router;
 
