37412ca4a75e1126aa6560dcf7f99b12ccd7ff74
flubbid <brendanwilson1@gmail.com>
2019-05-29 16:09:16 -0500

    add ingredient controller file

---
+++ controllers/ingredients.js
@@ -0,0 +1,20 @@
+const Recipe = require('../models/recipe');
+const Ingredients = require('../models/ingredient')
+
+module.exports = {
+    new: newPerformer,
+    create,
+    addToRecipe
+}
+
+function newPerformer(req, res){
+
+}
+
+function create(req, res){
+
+}
+
+function addToRecipe (req, res){
+    
+}
