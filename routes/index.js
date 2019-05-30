53bd01d907cf8cb870a5db8c922d408d5abde989
flubbid <brendanwilson1@gmail.com>
2019-05-30 10:12:11 -0500

    corrected router for the userCtrl.index

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport')
-
+const userCtlr = require('../controllers/users');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+router.get('/', userCtlr.index);
 
 router.get('/auth/google', passport.authenticate('google', 
 { scope: ['profile', 'email']}
