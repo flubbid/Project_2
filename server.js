93bb58e95155578431bed18401de400666d83557
flubbid <brendanwilson1@gmail.com>
2019-05-31 07:53:27 -0500

    adds method override to the server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var passport = require('passport')
+var passport = require('passport');
+var methodOverride = require('method-override');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,9 +23,9 @@ require('./config/database')
 require('./config/passport')
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.set('views', path.join(__dirname, 'views'));
+app.use(methodOverride('_method'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
