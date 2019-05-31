3ce26989dd08a74bf144081852e78a8f18ad90f6
flubbid <brendanwilson1@gmail.com>
2019-05-31 07:52:40 -0500

    adds update, addingredient, and destroy routes to the recipe router

--- routes/recipes.js
+++ routes/recipes.js
@@ -5,6 +5,15 @@ router.get( '/', recipeCtrl.index)
 router.get('/new', recipeCtrl.new)
 router.post('/', recipeCtrl.create)
 router.get('/:id', recipeCtrl.show)
+router.get('/:id/detail', recipeCtrl.detail)
+router.get('/:id/edit', recipeCtrl.edit)
+router.put('/:id', recipeCtrl.update)
+router.post('/:id/ingredients', recipeCtrl.addIngredient)
+router.delete('/:id', isLoggedIn, recipeCtrl.destroy)
 
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect('/');
+}
 
 module.exports = router;
