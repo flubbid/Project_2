f90dec2e28a76ddc56d6608cd0b5bef4d1d8b631
flubbid <brendanwilson1@gmail.com>
2019-05-31 07:43:23 -0500

    adds destroy function, update function, and edit function

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -5,7 +5,12 @@ module.exports = {
     index,
     show,
     new: newRecipe,
-    create
+    create,
+    detail,
+    addIngredient,
+    edit,
+    update,
+    destroy
 }
 
 function index (req, res){
@@ -19,11 +24,10 @@ function index (req, res){
 
 function show(req, res){
  Recipe.findById(req.params.id)
- .populate('ingredient').exec(function (err, recipe){
+ .populate('ingredients').exec(function (err, recipe){
      Ingredient.find({_id: {$nin: recipe.ingredients}}).exec(function(err, ingredients){
-         console.log(ingredients);
          res.render('recipes/show', {
-             title: 'Recipe Detail', recipe, ingredients
+             title: 'Recipe Detail', recipe, ingredients, user: req.user
          });
      });
  });
@@ -31,7 +35,7 @@ function show(req, res){
 
 
 function newRecipe(req, res){
-res.render('recipes/new', { title: 'Add Recipe'})
+res.render('recipes/new', { title: 'Add Recipe', user: req.user})
 }
 
 function create (req, res){
@@ -39,4 +43,35 @@ Recipe.create(req.body, function(err, recipe){
     if (err) return res.redirect('/recipes/new');
     res.redirect(`/recipes/${recipe._id}`)
 })
+}
+
+function detail (req,res){
+    Recipe.findById(req.params.id).populate('ingredients').exec(function(err, recipe){
+        res.render('recipes/detail', {title: 'Add Recipe', user: req.user, recipe})
+    })
+}
+
+function addIngredient(req, res) {
+    Recipe.findByIdAndUpdate(req.params.id, { $push: { ingredients: req.body.id}}, {new: true}, function(err, recipe) {
+        console.log(err)
+        res.redirect(`/recipes/${req.params.id}`)
+    })
+}
+
+function edit(req, res){
+    Recipe.findById(req.params.id, function(err, recipe) {
+        res.render('recipes/edit', { user: req.user, recipe });
+    });
+}
+
+function update(req, res){
+    Recipe.findByIdAndUpdate(req.params.id, req.body, {new: true}, function(err, updatedRecipe) {
+        res.redirect(`/recipes/${req.params.id}`);
+    });
+}
+
+function destroy(req, res){
+    Recipe.findByIdAndDelete(req.params.id, function(err){
+        res.redirect(`/recipes`)
+    })
 }
